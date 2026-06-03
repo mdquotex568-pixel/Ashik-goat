fad5c90d130def27d45aa7133281769627a52979
mdquotex568-pixel <mdquotex568@gmail.com>
2026-06-03 21:58:04 +0600

    Update Cow.js

--- scripts/cmds/Cow.js
+++ scripts/cmds/Cow.js
@@ -12,7 +12,7 @@ module.exports = {
                 name: "cow",
                 version: "1.7",
                 author: "MahMUD",
-                role: 0,
+                role: 2,
                 category: "fun",
                 cooldown: 10,
                 guide: {
